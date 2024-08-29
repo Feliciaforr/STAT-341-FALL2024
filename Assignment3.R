5d5a678fc6cd8cf7a1fc9237641388ae85d51293
Feliciaforr <124526337+Feliciaforr@users.noreply.github.com>
2024-08-29 15:13:03 -0500

    Add files via upload

---
+++ Assignment3.R
@@ -0,0 +1,20 @@
+#Author: Felicia Forrester, Date:08/29/24, Purpose: Creating 1000 random numbers and calculating the sd,mean & median
+
+#We use this function to create 1000 random numbers
+
+rnorm(1000)
+
+# Storing the random numbers in a variable
+
+variable1 <-rnorm(1000)
+
+#Calculating the standard deviation
+
+sd(variable1)
+
+#Calculating the mean
+
+mean(variable1)
+
+# Calculating the median
+median(variable1)
