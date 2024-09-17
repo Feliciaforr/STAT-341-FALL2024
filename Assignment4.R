fe296ca0a4d341f75d971deee2a889e68da72a9a
Feliciaforr <124526337+Feliciaforr@users.noreply.github.com>
2024-09-17 15:03:52 -0500

    Add files via upload

---
+++ Assignment4.R
@@ -0,0 +1,14 @@
+#Felicia Forrester, Date: 9/17/2024, Purpose: Correlation Test
+
+
+#Read mtcars dataset in  R
+
+mtcars
+
+#Assign the data to a variable
+
+mydata<-mtcars
+
+# Calculate Correlation Coefficient Value
+
+cor(my_data$wt,my_data$gear)
